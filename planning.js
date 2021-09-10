7e628dbc8b8da09a13ed3ad78bac5219790e6923
Bhawna Kumari <bhawnakumari.7870@gmail.com>
2021-09-10 18:34:51 +0530

    Planning Done

---
+++ planning.js
@@ -0,0 +1,22 @@
+// Book My Show planning
+
+// -> We need a reusable carousal
+// ->Hero Carousal
+// ->Common Carousal
+// ->Cast and Crew small Carousal
+
+
+
+//Pages
+
+// -> Home Page
+// -> Movie Page
+// ->Categories Pages
+// ->Auth Page
+// -> Razor Payment GateWay
+
+//Plan-> How are we building????🤔
+//Layouts 
+//Routes
+//Pages
+//Components
