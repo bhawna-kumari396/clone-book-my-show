61eca20b938db367f9d12c926722831b93ad9aad
Bhawna Kumari <bhawnakumari.7870@gmail.com>
2021-10-07 16:51:10 +0530

    Add movie page poster and component

--- src/components/MovieHero/MovieHero.component.jsx
+++ src/components/MovieHero/MovieHero.component.jsx
@@ -1,23 +1,34 @@
 import React from "react";
+import MovieInfo from "./MovieInfo.component";
 
 const MovieHero = () => {
   return (
     <>
       <div>
-        <div className="md:hidden w-full" style={{ height: "calc(180vw)" }}>
-          <div className="w-full h-56 bg-opacity-50 absolute bg-black z-10 bottom-0" />
+        {/* mobile */}
+        <div className="relative md:hidden w-full" style={{ height: "calc(180vw)" }}>
+         
+          <div className="absolute z-20 bottom-4 left-4">
+            <MovieInfo />
+          </div>
 
+
+          <div className="w-full h-56 bg-opacity-50 absolute bg-black z-10 bottom-0" />
           <img
             src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/proximity-et00302306-13-05-2021-10-06-47.jpg"
             alt="poster"
             className="w-full h-full"
           />
         </div>
-
+        
+        {/*Medium*/}
         <div
           className="relative hidden md:block lg:hidden"
-          style={{ height: "calc(180vw)" }}
+          style={{ height: "calc(100vw)" }}
         >
+          <div className="absolute z-20 bottom-4">
+            <MovieInfo />
+          </div>
           <div className="w-full h-56 bg-opacity-50 absolute bg-black z-10 bottom-0" />
           <img
             src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/proximity-et00302306-13-05-2021-10-06-47.jpg"
@@ -25,7 +36,10 @@ const MovieHero = () => {
             className="w-full h-full"
           />
         </div>
+        
+        {/*Large*/}
         <div className="relative hidden lg:block" style={{ height: "30rem" }}>
+          
           <div
             className="absolute z-10 w-full h-full"
             style={{
@@ -33,12 +47,17 @@ const MovieHero = () => {
                 "linear-gradient(90deg, rgb(34, 34, 34) 24.97%, rgb(34, 34, 34) 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgb(34, 34, 34) 100%)",
             }}
           />
-          <div className="absolute z-30 w-64 h-96 left-24 top-10">
-            <img
-              src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/proximity-et00302306-13-05-2021-10-06-47.jpg"
-              alt="poster"
-              className="w-full h-full"
-            />
+          <div className="absolute z-30 left-24 top-10 flex items-center gap-10">
+            <div className="w-64 h-96">
+              <img
+                src="https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/proximity-et00302306-13-05-2021-10-06-47.jpg"
+                alt="poster"
+                className="w-full h-full rounded-xl"
+              />
+            </div>
+            <div>
+              <MovieInfo />
+            </div>
           </div>
           <img
             src="https://in.bmscdn.com/iedb/movies/images/extra/horizontal_no_logo/mobile/listing/xxlarge/proximity-et00302306-13-05-2021-10-06-47.jpg"
--- src/pages/Movie.pages.jsx
+++ src/pages/Movie.pages.jsx
@@ -8,7 +8,7 @@ const Movie = () => {
         <MovieHero />
           
         </>
-    )
+    );
 };
 
 export default Movie;
